fc2f2fac40dc04e218ec6914f22f56cf02efbc83
Luis Navarro <luisfnvrr@gmail.com>
2020-09-08 15:19:56 -0300

    agrego consultar usuarios

---
+++ Formularios/Abm/FrmConsultarUsuarios.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Practico.Clases;
+using Practico.Negocios;
+
+namespace Practico.Formularios.Abm.Usuarios
+{
+    public partial class FrmModificarUsuarios : Form
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string contraseña { get; set; }
+
+
+        public FrmModificarUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmModificarUsuarios_Load(object sender, EventArgs e)
+        {
+            lblId.TabStop = false;
+            lblNombre.TabStop = false;
+            lblContraseña.TabStop = false;
+            lblRepetir.TabStop = false;
+            lblNuevo.TabStop = false;
+            CargarCampos();
+            
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e) //es boton aceptar
+        {
+            TratamientosEspeciales tratamiento = new TratamientosEspeciales();
+
+            if (tratamiento.Validar(this.Controls) == TratamientosEspeciales.Validacion.correcta)
+            {
+                if (txtContraseña.Text == txtRepetir.Text)
+                {
+                    Negocios.Usuarios usuarios = new Negocios.Usuarios();
+                    usuarios.ModificarUsuario(Int32.Parse(id), txtNombre.Text.Trim(), txtContraseña.Text.Trim());
+                    MessageBox.Show("Usuario modificado correctamente", "Informacion",
+                        buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Information);
+                    CargarCampos();
+                }
+                else
+                {
+                    MessageBox.Show("Las contraseñas no coinciden!", "Importante", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    LimpiarContraseñas();
+                    txtContraseña.Focus();
+                }
+            }
+        }
+
+        // limpiar textbox
+        private void LimpiarContraseñas()
+        {
+            txtContraseña.Text = "";
+            txtRepetir.Text = "";
+            txtContraseña.Focus();
+        }
+
+        private void LimpiarCampos()
+        {
+            LimpiarContraseñas();
+            txtNombre.Text = "";
+            txtNombre.Focus();
+        }
+
+
+        // ENTER funciona como TAB
+        private void txtNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                SelectNextControl(ActiveControl, true, true, true, true);
+            }
+        }
+
+        private void txtContraseña_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                SelectNextControl(ActiveControl, true, true, true, true);
+            }
+        }
+
+        private void txtRepetir_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                SelectNextControl(ActiveControl, true, true, true, true);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void CargarCampos()
+        {
+            Negocios.Usuarios usuario = new Negocios.Usuarios();
+            DataTable tabla = usuario.RecuperarUsuario(Int32.Parse(id));
+            id = tabla.Rows[0]["idUsuario"].ToString();
+            nombre = tabla.Rows[0]["nombreUsuario"].ToString();
+            contraseña = tabla.Rows[0]["contrasenia"].ToString();
+
+            txtId.Text = id;
+            txtNombre.Text = nombre;
+            txtContraseña.Text = contraseña;
+        }
+    }
+}
---
+++ Formularios/Abm/FrmModificarUsuarios.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Practico.Formularios.Abm.Usuarios
+{
+    partial class FrmModificarUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmModificarUsuarios));
+            this.lblNuevo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.lblRepetir = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txtRepetir = new Practico.Clases.TextBox01();
+            this.txtContraseña = new Practico.Clases.TextBox01();
+            this.txtNombre = new Practico.Clases.TextBox01();
+            this.txtId = new Practico.Clases.TextBox01();
+            this.lblId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNuevo
+            // 
+            this.lblNuevo.AutoSize = true;
+            this.lblNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblNuevo.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.lblNuevo.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblNuevo.Location = new System.Drawing.Point(22, 34);
+            this.lblNuevo.Name = "lblNuevo";
+            this.lblNuevo.Size = new System.Drawing.Size(174, 23);
+            this.lblNuevo.TabIndex = 7;
+            this.lblNuevo.Text = "Modificar Usuario";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblNombre.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblNombre.Location = new System.Drawing.Point(105, 117);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(68, 19);
+            this.lblNombre.TabIndex = 9;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblContraseña.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.lblContraseña.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblContraseña.Location = new System.Drawing.Point(79, 147);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(94, 19);
+            this.lblContraseña.TabIndex = 11;
+            this.lblContraseña.Text = "Contraseña:";
+            // 
+            // lblRepetir
+            // 
+            this.lblRepetir.AutoSize = true;
+            this.lblRepetir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblRepetir.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.lblRepetir.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblRepetir.Location = new System.Drawing.Point(26, 177);
+            this.lblRepetir.Name = "lblRepetir";
+            this.lblRepetir.Size = new System.Drawing.Size(147, 19);
+            this.lblRepetir.TabIndex = 13;
+            this.lblRepetir.Text = "Repetir Contraseña:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.btnCancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCancelar.Location = new System.Drawing.Point(89, 219);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(103, 40);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.btnAceptar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.btnAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAceptar.Location = new System.Drawing.Point(200, 219);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(103, 40);
+            this.btnAceptar.TabIndex = 3;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // txtRepetir
+            // 
+            this.txtRepetir.BackColor = System.Drawing.Color.White;
+            this.txtRepetir.Location = new System.Drawing.Point(179, 172);
+            this.txtRepetir.Name = "txtRepetir";
+            this.txtRepetir.PasswordChar = '*';
+            this.txtRepetir.PpMensajeError = "No repitió contraseña!";
+            this.txtRepetir.PpNombreCampo = null;
+            this.txtRepetir.PpNombreTabla = null;
+            this.txtRepetir.PpValidable = true;
+            this.txtRepetir.Size = new System.Drawing.Size(124, 24);
+            this.txtRepetir.TabIndex = 2;
+            this.txtRepetir.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtRepetir_KeyDown);
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.BackColor = System.Drawing.Color.White;
+            this.txtContraseña.Location = new System.Drawing.Point(179, 142);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '*';
+            this.txtContraseña.PpMensajeError = "No ingresó contraseña!";
+            this.txtContraseña.PpNombreCampo = null;
+            this.txtContraseña.PpNombreTabla = null;
+            this.txtContraseña.PpValidable = true;
+            this.txtContraseña.Size = new System.Drawing.Size(124, 24);
+            this.txtContraseña.TabIndex = 1;
+            this.txtContraseña.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtContraseña_KeyDown);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.White;
+            this.txtNombre.Location = new System.Drawing.Point(179, 112);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.PpMensajeError = "No ingresó nombre!";
+            this.txtNombre.PpNombreCampo = null;
+            this.txtNombre.PpNombreTabla = null;
+            this.txtNombre.PpValidable = true;
+            this.txtNombre.Size = new System.Drawing.Size(124, 24);
+            this.txtNombre.TabIndex = 0;
+            this.txtNombre.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNombre_KeyDown);
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.Color.DarkGray;
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(179, 82);
+            this.txtId.Name = "txtId";
+            this.txtId.PpMensajeError = "No ingresó nombre!";
+            this.txtId.PpNombreCampo = null;
+            this.txtId.PpNombreTabla = null;
+            this.txtId.PpValidable = true;
+            this.txtId.Size = new System.Drawing.Size(124, 24);
+            this.txtId.TabIndex = 14;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblId.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.lblId.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblId.Location = new System.Drawing.Point(147, 85);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(26, 19);
+            this.lblId.TabIndex = 15;
+            this.lblId.Text = "Id:";
+            // 
+            // FrmModificarUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.ClientSize = new System.Drawing.Size(388, 321);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtRepetir);
+            this.Controls.Add(this.lblRepetir);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblNuevo);
+            this.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmModificarUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registrar Usuario";
+            this.Load += new System.EventHandler(this.FrmModificarUsuarios_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblNuevo;
+        private Clases.TextBox01 txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private Clases.TextBox01 txtContraseña;
+        private System.Windows.Forms.Label lblContraseña;
+        private Clases.TextBox01 txtRepetir;
+        private System.Windows.Forms.Label lblRepetir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private Clases.TextBox01 txtId;
+        private System.Windows.Forms.Label lblId;
+    }
+}
