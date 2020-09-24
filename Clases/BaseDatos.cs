068ae63e8f6a752696dc619508f7ae9b6c76a2a3
nicolas1819991@gmail.com <nicolas1819991@gmail.com>
2020-09-24 17:11:50 -0300

    .

--- Clases/BaseDatos.cs
+++ Clases/BaseDatos.cs
@@ -22,13 +22,13 @@ namespace Practico.Clases
         public TipoConexion controlConexion { get; set; } = TipoConexion.simple;
         public EstadoTransaccion controlTransaccion { get; set; } = EstadoTransaccion.correcta;
 
-        string cadenaConexion = "Data Source=DESKTOP-6V98254\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; // LUIS
-        //string cadenaConexion = "Data Source=DESKTOP-L73414Q\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //NICO
+        //string cadenaConexion = "Data Source=DESKTOP-6V98254\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; // LUIS
+        string cadenaConexion = "Data Source=DESKTOP-DT487OK\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //NICO
         //string cadenaConexion = "Data Source=DESKTOP-49CNN6T\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //MARCOS
         //string cadenaConexion = "Data Source=DESKTOP-5U8R5O6\\SQLEXPRESS; Initial Catalog=HeladeriaPAV;Integrated Security=True"; //GASTÓN
         //string cadenaConexion = "Data Source=DESKTOP-UOQ5CGK;Initial Catalog=HeladeriaPAV;Integrated Security=True"; // Bruno
 
-        
+
 
         public void IniciarTransaccion()
         {
@@ -60,9 +60,8 @@ namespace Practico.Clases
 
         private void Conectar()  // metodo para conectar
         {
-            conexion.ConnectionString = "Data Source=DESKTOP-L73414Q\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //NICO
+            conexion.ConnectionString = "Data Source=DESKTOP-DT487OK\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //NICO
             //conexion.ConnectionString = "Data Source=DESKTOP-6V98254\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //LUIS
-            //conexion.ConnectionString = "Data Source=DESKTOP-L73414Q\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //NICO
             //conexion.ConnectionString = "Data Source=DESKTOP-6V98254\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //LUIS
             //conexion.ConnectionString = "Data Source=DESKTOP-49CNN6T\\SQLEXPRESS;Initial Catalog=HeladeriaPAV;Integrated Security=True"; //MARCOS
             //conexion.ConnectionString = "Data Source=DESKTOP-5U8R5O6\\SQLEXPRESS; Initial Catalog=HeladeriaPAV;Integrated Security=True"; //GASTÓN
